59bd51a24de8604a88b076db31dd180c1eb57b8c
rodten23 <rodten23@yahoo.com.br>
2022-08-14 23:33:29 -0300

    Início da criação da aplicação usando Express.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-console.log("Olá, Mundo!"); //Javascript aceita tanto aspas simples, como aspas duplas.
+//Javascript aceita tanto aspas simples, como aspas duplas.
+
+// Área da Preparação, onde cria uma Aplicação usando express.
+// Require indica que precisamos do pacote express e atribuo ele a variável também express, que é uma constante.
+// Versãos mais antigas do JavaScrip usa-se "formato" Require, mas nas versões mais novas usa-se o "formato" Import (VS Code corrigi automaticamente, se quiser).
+// Por fim, executa-se a nossa "função" express colocando () e, pelo padrão, atribui-se ela à variável constante app, que será nossa Aplicação.
+const express = require("express");
+const app = express();
+
+// Essa área cria o endpoint (rota) principal (/).
+// Aplicação (app) está preparada para receber um requesição (request) do método HTTP Get do navegador e então executa a function e com res.send devolve para esse Get "valor" (Oi, Mundão!).
+// Conteúdo (ações) da function fica entre {}.
+app.get("/", function (req, res) {
+    res.send("Oi, Mundão!");
+});
+
+// Rota alternativa (/ola) que também tem informação diferente (Olá, Mundo!).
+app.get("/ola", function (req, res) {
+    res.send("Olá, Mundo!");
+});
+
+// Aqui, aplicação está ouvindo qualquer solicitação na porta 3000 e ele conclui nossa aplicação, então tudo que quisermos criar e executar tem que vir antes deste listen.
+app.listen(3000);

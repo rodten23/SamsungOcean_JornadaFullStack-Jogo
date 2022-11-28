90f607114ae58f0a492dadf7c62dc1153d6a9649
rodten23 <rodten23@yahoo.com.br>
2022-11-28 02:03:36 -0300

    Adequação do gitigore, pequenas correções no index.js

--- backend/index.js
+++ backend/index.js
@@ -2,19 +2,21 @@
 
 // Área da Preparação, onde cria uma Aplicação usando express.
 // Require indica que precisamos do pacote express e atribuo ele a variável também express, que é uma constante.
-// Versãos mais antigas do JavaScrip usa-se "formato" Require, mas nas versões mais novas usa-se o "formato" Import (VS Code corrigi automaticamente, se quiser).
+// Versãos mais antigas do JavaScrip usa "formato" Require, mas nas versões mais novas usa-se o "formato" Import (VS Code corrige automaticamente, se quiser).
 // Por fim, executa-se a nossa "função" express colocando () e, pelo padrão, atribui-se ela à variável constante app, que será nossa Aplicação.
 const express = require("express");
+//import express from "express";
 const app = express();
 
 // Essa área cria o endpoint (rota) principal (/).
 // Aplicação (app) está preparada para receber um requesição (request) do método HTTP Get do navegador e então executa a function e com res.send devolve para esse Get "valor" (Oi, Mundão!).
 // Conteúdo (ações) da function fica entre {}.
+// Mesmo que não use um ou outro, o Express exige o "req, res", exatamente nessa ordem.
 app.get("/", function (req, res) {
     res.send("Oi, Mundão!");
 });
 
-// Rota alternativa (/ola) que também tem informação diferente (Olá, Mundo!).
+// Rota alternativa (/ola) que tem informação diferente (Olá, Mundo!).
 app.get("/ola", function (req, res) {
     res.send("Olá, Mundo!");
 });
